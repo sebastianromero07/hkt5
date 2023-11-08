e2b9ea13759c688215af44e27b866bce081e0565
sebastianromero07 <112996148+sebastianromero07@users.noreply.github.com>
2023-11-07 22:11:47 -0600

    Delete HCK1 directory

--- HCK1/src/main/java/com/example/hck1/Grupo/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hck1.Grupo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    Optional<Group> findById(Long id);
-}
--- HCK1/src/main/java/com/example/hck1/TiposGrupos/TiposgruposRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hck1.TiposGrupos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface TiposgruposRepository extends JpaRepository<Tiposgrupos, Long> {
-    Optional<Tiposgrupos> findById(Long id);
-}
